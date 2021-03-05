cb88de51c80c070d9af519cffd4e702d3f0ae268
beautiful nature <heimarunag@gmail.com>
2021-03-05 06:55:43 +0530

    done

---
+++ jpa/jpa-relation/mypractice/queryEx/src/test/java/myorg/queryex/QueryBase.java
@@ -0,0 +1,83 @@
+package myorg.queryex;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+
+public class QueryBase {
+    private static Logger log = LoggerFactory.getLogger(QueryBase.class);
+    private static final String PERSISTENCE_UNIT = "queryEx-test";
+    protected static EntityManagerFactory emf;
+    protected EntityManager em;    
+
+    @BeforeClass
+    public static void setUpClass() {
+        log.debug("creating entity manager factory");
+        emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+        EntityManager em1 = emf.createEntityManager();
+        cleanup(em1);
+        populate(em1);
+    }
+    
+    @Before
+    public void setUp() throws Exception {
+        log.debug("creating entity manager");
+        em = emf.createEntityManager();
+        em.getTransaction().begin();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        log.debug("tearDown() started, em={}", em);
+        if (em!=null) {
+            EntityTransaction tx = em.getTransaction();
+            if (tx.isActive()) {
+                if (tx.getRollbackOnly() == true) { tx.rollback(); }
+                else                              { tx.commit(); }
+            } else {
+                tx.begin();
+                tx.commit();
+            }
+            em.close();
+            em=null;
+        }
+        log.debug("tearDown() complete, em={}", em);
+     }
+    
+    @AfterClass
+    public static void tearDownClass() {
+        log.debug("closing entity manager factory");
+        if (emf!=null) { emf.close(); }
+    }
+    
+    public static void cleanup(EntityManager em) {
+        	em.getTransaction().begin();
+        	for (Movie movie : em.createQuery("from Movie", Movie.class).getResultList()) {
+        		em.remove(movie);
+        	}
+        	for (Actor actor: em.createQuery("from Actor", Actor.class).getResultList()) {
+        		em.remove(actor);
+        	}
+        	for (Director director: em.createQuery("from Director", Director.class).getResultList()) {
+        		em.remove(director);
+        	}
+        	for (Person person: em.createQuery("from Person", Person.class).getResultList()) {
+        		em.remove(person);
+        	}
+        	em.getTransaction().commit();
+    }
+    
+    public static void populate(EntityManager em) {
+        	em.getTransaction().begin();
+        	new MovieFactory().setEntityManager(em).populate();
+        	em.getTransaction().commit();
+    }
+}
